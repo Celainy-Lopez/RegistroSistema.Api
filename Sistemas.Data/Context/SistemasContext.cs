6154c2de4777be36297ce173a3b3a83ef100cdfd
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 10:14:54 -0400

    Creación del contexto

--- Sistemas.Data/Context/SistemasContext.cs
+++ Sistemas.Data/Context/SistemasContext.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sistemas.Data.Models;
+namespace Sistemas.Data.Context;
 
-namespace Sistemas.Data.Context
+public class SistemasContext : DbContext
 {
-    internal class SistemasContext
-    {
-    }
+    public SistemasContext(DbContextOptions<SistemasContext> options) : base(options) { }
+    public DbSet<Sistema> Sistemas { get; set; }
 }
