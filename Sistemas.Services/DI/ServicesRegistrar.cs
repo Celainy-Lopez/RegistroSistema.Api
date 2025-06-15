f9becd2b976b0e002916a7373e12f4530590d672
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 15:42:30 -0400

    Inyectando el servicio

--- Sistemas.Services/DI/ServicesRegistrar.cs
+++ Sistemas.Services/DI/ServicesRegistrar.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sistemas.Abstractions;
+using Sistemas.Data.DI;
 
-namespace Sistemas.Services.DI
+namespace Sistemas.Services.DI;
+
+public static class ServicesRegistrar
 {
-    internal class ServicesRegistrar
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        services.RegisterDbContextFactory();
+        services.AddScoped<ISistemasService, SistemasService>();
+        return services;
     }
+
 }
