f0e0fb85a7f5a87b191c3e9fac4b90f50e7b58dd
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 10:10:41 -0400

    Creación del modelo

--- Sistemas.Data/Models/Sistemas.cs
+++ Sistemas.Data/Models/Sistemas.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sistemas.Data.Models
+namespace Sistemas.Data.Models;
+
+public class Sistema
 {
-    internal class Sistemas
-    {
-    }
+    [Key]
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "Ingrese una descripcion valido")]
+    [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "La descripcion del sistema solo debe contener letras.")]
+
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Ingrese un costo valido")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "El costo debe ser mayor que 0.")]
+    public double Costo { get; set; }
 }
