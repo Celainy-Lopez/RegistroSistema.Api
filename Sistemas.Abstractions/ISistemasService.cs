cbdc4353928b0af01004f4fa963a4ec0dd46422d
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 15:44:10 -0400

    Creando interfaz del servicio

--- Sistemas.Abstractions/ISistemasService.cs
+++ Sistemas.Abstractions/ISistemasService.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Sistemas.Domain.DTO;
 
-namespace Sistemas.Abstractions
+namespace Sistemas.Abstractions;
+
+public interface ISistemasService
 {
-    internal class ISistemasService
-    {
-    }
+    Task<bool> Guardar(SistemasDto cliente);
+    Task<bool> Eliminar(int sistemaId);
+    Task<SistemasDto> Buscar(int id);
+    Task<List<SistemasDto>> Listar(Expression<Func<SistemasDto, bool>> criterio);
+    Task<bool> ExisteSistema(int id, string nombre);
+
 }
