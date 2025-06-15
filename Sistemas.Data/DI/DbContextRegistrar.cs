cb3937d166938c92966437af4bf6c1ed81c15283
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 11:12:24 -0400

    Creando DbContexFactory

--- Sistemas.Data/DI/DbContextRegistrar.cs
+++ Sistemas.Data/DI/DbContextRegistrar.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sistemas.Data.Context;
 
-namespace Sistemas.Data.DI
+namespace Sistemas.Data.DI;
+
+public static class DbContextRegistrar
 {
-    internal class DbContextRegistrar
+    public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
     {
+        services.AddDbContextFactory<SistemasContext>(o => o.UseSqlServer("Name=SqlConStr"));
+        return services;
     }
 }
